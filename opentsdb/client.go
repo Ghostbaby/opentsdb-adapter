efe68f6f9f9c00d6bbec30309f484e91ab3d2b36
yueji <xzg_upc@163.com>
2018-12-20 11:54:59 +0800

    Update client.go

--- opentsdb/client.go
+++ opentsdb/client.go
@@ -32,7 +32,7 @@ import (
 
 	"github.com/go-kit/kit/log/level"
 
-	"github.com/caitong93/opentsdb-adapter/prompb"
+	"github.com/yueji12321/opentsdb-adapter/prompb"
 	"github.com/go-kit/kit/log"
 	"github.com/prometheus/common/model"
 	"golang.org/x/net/context/ctxhttp"
